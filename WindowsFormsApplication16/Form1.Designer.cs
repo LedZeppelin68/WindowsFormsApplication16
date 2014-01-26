1f995933e8d8866ca8f3de8e0e409d70a2e7d704
LedZeppelin68 <ledzeppelin68@gmail.com>
2014-01-26 18:16:01 +0400

    Initial

---
+++ WindowsFormsApplication16/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.2F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(814, 559);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(808, 443);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 73;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 73;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 73;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 559);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ WindowsFormsApplication16/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var rawdump = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            var rawreader = new BinaryReader(new MemoryStream(File.ReadAllBytes(rawdump[0])));
+
+            while (rawreader.BaseStream.Position != rawreader.BaseStream.Length)
+            {
+                var rawopcode = rawreader.ReadInt32();
+
+                var optype = translaterawopcode(rawopcode);
+
+                var rawopcodehexstring = string.Format("0x{0:X8}", rawopcode);
+
+                var debug = string.Format("{0}  {1}", rawopcode >> 26 & 0x3f, rawopcode & 0x3f);
+                dataGridView1.Rows.Add(rawopcodehexstring, optype.asm, debug);
+            }
+            
+        }
+
+        string[] register = {"r0", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra" };
+
+        struct translatedopcode
+        {
+            public string asm;
+            public string sharp;
+        }
+
+        struct Rinstr
+        {
+            public string opcode;
+            public string rs;
+            public string rt;
+            public string rd;
+            public int shamt;
+            public int sa;
+            public string funct;
+        }
+
+        struct Iinstr
+        {
+            public string opcode;
+            public string rs;
+            public string rt;
+            public int immediate;
+            public int offset;
+            public string _base;
+        }
+
+        struct Jinstr
+        {
+            public string opcode;
+            public string address;
+        }
+
+        Rinstr getRinstr(int rawopcode)
+        {
+            var instr = new Rinstr();
+            instr.shamt = rawopcode >> 6 & 0x1f;
+            instr.sa = rawopcode >> 6 & 0x1f;
+            instr.rd = register[rawopcode >> 11 & 0x1f];
+            instr.rt = register[rawopcode >> 16 & 0x1f];
+            instr.rs = register[rawopcode >> 21 & 0x1f];
+            return instr;
+        }
+
+        Iinstr getIinstr(int rawopcode)
+        {
+            var instr = new Iinstr();
+            instr.immediate = (Int16)(rawopcode & 0xffff);
+            instr.offset = (Int16)(rawopcode & 0xffff);
+            instr.rt = register[rawopcode >> 16 & 0x1f];
+            instr.rs = register[rawopcode >> 21 & 0x1f];
+            instr._base = register[rawopcode >> 21 & 0x1f];
+            return instr;
+        }
+
+        translatedopcode translaterawopcode(int rawopcode)
+        {
+            var type = rawopcode >> 26;
+            var optype = new translatedopcode();
+            switch(type)
+            {
+                case 0: optype.asm = "SPECIAL";
+                    var opcode = rawopcode & 0x3f;
+                    switch (opcode)
+                    {
+                        case 0://sll
+                            var instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x2}", "sll", instr.rd, instr.rt, instr.sa);
+                            break;
+                        case 2://srl
+                            instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x2}", "srl", instr.rd, instr.rt, instr.sa);
+                            break;
+                        case 4://sllv
+                            instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, {3}", "sllv", instr.rd, instr.rt, instr.rs);
+                            break;
+                        case 8://jr
+                            instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}", "jr", instr.rs);
+                            break;
+                        case 33://addu
+                            instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, {3}", "addu", instr.rd, instr.rs, instr.rt);
+                            break;
+                        case 37://or
+                            instr = getRinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, {3}", "or", instr.rd, instr.rs, instr.rt);
+                            break;
+                    }
+                    break;
+                case 1: optype.asm = "REGIMM";
+
+                    break;
+                default: optype.asm = "STANDARD";
+                    opcode = rawopcode >> 26 & 0x3f;
+                    switch (opcode)
+                    {
+                        case 5://bne
+                            var instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x4}", "bne", instr.rs, instr.rt, instr.offset);
+                            break;
+                        case 9://addiu
+                            instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x4}", "addiu", instr.rt, instr.rs, instr.immediate);
+                            break;
+                        case 10://slti
+                            instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x4}", "slti", instr.rt, instr.rs, instr.immediate);
+                            break;
+                        case 12://andi
+                            instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, {2}, ${3:x4}", "andi", instr.rt, instr.rs, instr.immediate);
+                            break;
+                        case 36://lbu
+                            instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, ${2:x4}({3})", "lbu", instr.rt, instr.offset, instr._base);
+                            break;
+                        case 40://sb
+                            instr = getIinstr(rawopcode);
+                            optype.asm = string.Format("{0,-6} {1}, ${2:x4}({3})", "sb", instr.rt, instr.offset, instr._base);
+                            break;
+                    }
+                    break;
+            }
+            return optype;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var cell = dataGridView1.SelectedCells[0];
+            if (cell.ColumnIndex == 0)
+            {
+                var opcode = Convert.ToInt32(cell.Value);
+                MessageBox.Show(opcode.ToString());
+            }
+        }
+    }
+}
